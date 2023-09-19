9ff4e4068705e29cbd61f3cb15030fe050cf4636
daviidhache <socialfreetime@gmail.com>
2023-09-19 20:12:56 +0200

    Agregar archivos de proyecto.

---
+++ TareaImagenes/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace TareaImagenes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.flp2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "The Simpons ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // flp2
+            // 
+            this.flp2.Location = new System.Drawing.Point(292, 42);
+            this.flp2.Name = "flp2";
+            this.flp2.Size = new System.Drawing.Size(461, 381);
+            this.flp2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flp2);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flp2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TareaImagenes/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TareaImagenes
+{
+    
+    public partial class Form1 : Form
+    {
+       
+         static int[] listImagenes = new int[20];
+        static ArrayList aleatorios = new ArrayList();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            //aleatorio();
+            cargar();
+            cargarImagenes();
+         
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cargarImagenes()
+        {
+            for (int i = 0; i < 20; i++) {
+                Button boton = new Button();
+                boton.Width = 60;
+                boton.Height = 60;
+                boton.BackgroundImage = Image.FromFile(@"C:\\Users\\usuario\\source\repos\\TareaImagenes\\imagenes\\" + listImagenes[i] + ".JPG");
+                boton.BackgroundImageLayout = ImageLayout.Zoom;
+                flp2.Controls.Add(boton);
+            }
+        }
+
+    
+
+       
+
+       
+       
+
+       
+
+        // Debería funcionar
+        private void aleatorio() {
+            Random aleatorio = new Random();
+            for (int i = 0; i < listImagenes.Length; i++) {
+                int numAleatorio = aleatorio.Next(0, 19);
+                while (aleatorios.Contains(numAleatorio)) {
+                    numAleatorio = aleatorio.Next(0, 19);
+                }
+                listImagenes[i] = numAleatorio;
+                aleatorios.Add(numAleatorio);
+            }
+            
+        }
+
+        private void cargar() {
+            for (int i = 0; i < listImagenes.Length; i++) {
+                listImagenes[i] = i;
+            }
+        }
+
+      
+    }
+
+
+    }
+
+    
+  
+
+
+

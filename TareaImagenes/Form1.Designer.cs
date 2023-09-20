5d05fefed88ba1d80d278eefc6914c5a4ab032da
daviidhache <socialfreetime@gmail.com>
2023-09-20 17:34:07 +0200

    Juego simpson

--- TareaImagenes/Form1.Designer.cs
+++ TareaImagenes/Form1.Designer.cs
@@ -33,17 +33,19 @@ namespace TareaImagenes
             this.label2 = new System.Windows.Forms.Label();
             this.flp2 = new System.Windows.Forms.FlowLayoutPanel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bauhaus 93", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gold;
             this.label2.Location = new System.Drawing.Point(61, 55);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 17);
+            this.label2.Size = new System.Drawing.Size(177, 103);
             this.label2.TabIndex = 1;
-            this.label2.Text = "The Simpons ";
-   
+            this.label2.Text = "The SIMPSON";
             // 
             // flp2
             // 
@@ -52,18 +54,41 @@ namespace TareaImagenes
             this.flp2.Size = new System.Drawing.Size(461, 381);
             this.flp2.TabIndex = 3;
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(78, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 47);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(49, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 156);
+            this.label1.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.flp2);
             this.Controls.Add(this.label2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -72,6 +97,8 @@ namespace TareaImagenes
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.FlowLayoutPanel flp2;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- TareaImagenes/Form1.cs
+++ TareaImagenes/Form1.cs
@@ -17,6 +17,7 @@ namespace TareaImagenes
        
          static int[] listImagenes = new int[20];
         static ArrayList aleatorios = new ArrayList();
+         static int salida;
         
 
         public Form1()
@@ -24,6 +25,8 @@ namespace TareaImagenes
             InitializeComponent();
             aleatorio();
             cargarImagenes();
+            button1.Enabled = false;
+            timer1.Start();
          
         }
 
@@ -33,52 +36,110 @@ namespace TareaImagenes
         {
             for (int i = 0; i < 20; i++) {
                 Button boton = new Button();
+                boton.Click += Boton_Click;
                 boton.Width = 60;
                 boton.Height = 60;
                 boton.BackgroundImage = Image.FromFile(@"imagenes\" + listImagenes[i] + ".JPG");
+                boton.Tag = listImagenes[i];
                 boton.BackgroundImageLayout = ImageLayout.Zoom;
                 flp2.Controls.Add(boton);
+
+               
             }
         }
 
-       
-
+        private void Boton_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
             
-        
-        
+                
+                if (salida == Convert.ToInt32(boton.Tag))
+                {
+                    boton.BackgroundImage = Image.FromFile(@"imagenes\" + listImagenes[Convert.ToInt16(boton.Tag)] + ".JPG");
+                    MessageBox.Show("Me encontraste");
+                timer1.Start();
+                }
+                else {
+                    boton.BackgroundImage = Image.FromFile(@"imagenes\" + listImagenes[Convert.ToInt16(boton.Tag)] + ".JPG");
+                    MessageBox.Show("Fallaste");
+                    boton.BackgroundImage = Image.FromFile(@"back.png");
 
-    
+            }
 
-       
+                
+            
+          
+            
+        }
 
-       
-       
 
-       
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         // Debería funcionar
         private void aleatorio() {
             Random aleatorio = new Random();
             for (int i = 0; i < listImagenes.Length; i++) {
-                int numAleatorio = aleatorio.Next(0, 19);
+                int numAleatorio = aleatorio.Next(0, 20);
                 while (aleatorios.Contains(numAleatorio)) {
-                    numAleatorio = aleatorio.Next(0, 19);
+                    numAleatorio = aleatorio.Next(0, 20);
                 }
                 listImagenes[i] = numAleatorio;
                 aleatorios.Add(numAleatorio);
                
             }
+
+           
            
         }
 
 
-        
-                
-                    
+
+        private void imagenadivinar()
+        {
+
+            salida = new Random().Next(0, 20);
+            label1.Image = Image.FromFile(@"imagenes\" + listImagenes[salida]   + ".JPG");
+            label1.Enabled = true;
+
 
            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            imagenadivinar();
+            button1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Enabled = false;
+            foreach (Control dato in flp2.Controls) { 
+                dato.BackgroundImage = Image.FromFile(@"back.png");
+                dato.BackgroundImageLayout = ImageLayout.Zoom;
+
+            }
+            timer1.Stop();
+            button1.Enabled = true;
 
         }
+
+
+
+       
+    }
 
 
 
